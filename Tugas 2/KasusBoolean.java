b958b47e8949eca6838a31cb5087b3c1f915ad8e
13020230287 - St. Raditha Rauf <13020230287@student.umi.ac.id>
2025-03-15 21:57:41 +0800

    Penambahan Tugas 2 - PBO

---
+++ Tugas 2/KasusBoolean.java
@@ -0,0 +1,16 @@
+public class KasusBoolean {
+	
+	public static void main(String[] args) {
+		Boolean bool;
+		bool = true;
+		if(bool) {
+			System.out.print("true\n");
+		} else
+			System.out.print("false\n");
+		if(!bool) {
+			System.out.print("salah\n");
+		} else
+			System.out.print("benar\n");
+	}
+}
+		
